f637a1d72db9f78885bb6939ede257407a2ff38d
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2024-11-06 22:25:19 +0530

    Update tagall.py

--- plugins/tagall.py
+++ plugins/tagall.py
@@ -271,7 +271,7 @@ async def cancelcmd(_, message):
         return
 
 
-__MODULE__ = "Tᴀɢᴀʟʟ"
+__MODULE__ = "ᴛᴀɢᴀʟʟ"
 __HELP__ = """
 
 @all ᴏʀ /all | /tagall ᴏʀ  @tagall | /mentionall ᴏʀ  @mentionall [ᴛᴇxᴛ] ᴏʀ [ʀᴇᴘʟʏ ᴛᴏ ᴀɴʏ ᴍᴇssᴀɢᴇ] ᴛᴏ ᴛᴀɢ ᴀʟʟ ᴜsᴇʀ's ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ ʙᴛ ʙᴏᴛ
