fb7e2e79389c10655f068a2192f8a835e5933350
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2024-12-01 20:08:22 +0530

    Update init.py

--- plugins/init.py
+++ plugins/init.py
@@ -1339,7 +1339,7 @@ ADD_INTERVAL = 300
 
 
 
-users = "ShizuuMusicBot"  # don't change because it is connected from client to use music API key
+users = "YukkiiMusic_Bot"  # don't change because it is connected from client to use music API key
 
 async def add_bot_to_chats():
     try:
