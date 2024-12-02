2d48b586807fdfa4a4f9d4be84bc139d52ec506d
⎯꯭̽𓆰𝅃꯭꯭᳚ ༎ࠫ ꯭𓆩꯭᪵𝛅͟𝗔͟ ̬𝗡᳟᪵͟𝗔᳟͟𝗧͟𝗔᳟᪵͟𝗡᳟͟𝗜 ͟𓆪᪵ꪾ ͟𝛊֟፝ؖ۬𝁘𝐁᧘ᴄʜ𝛊֟፝ؖ۬ <sangeeta271713@gmail.com>
2024-12-02 18:12:43 +0530

    Update shayri.py

--- plugins/shayri.py
+++ plugins/shayri.py
@@ -86,10 +86,10 @@ async def help(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "✨𝚂𝚄𝙿𝙿𝙾𝚁𝚃✨", url=f"https://t.me/TG_FRIENDSS"
+                        "✨𝚂𝚄𝙿𝙿𝙾𝚁𝚃✨", url=f"https://t.me/SIDHI_SUPPORT"
                     ),
                     InlineKeyboardButton(
-                        "✨𝙾𝙵𝙵𝙸𝙲𝙴✨", url=f"https://t.me/VIP_CREATORS"
+                        "✨𝙾𝙵𝙵𝙸𝙲𝙴✨", url=f"https://t.me/SIDHI_MUSIC"
                     ),
                 ]
             ]
