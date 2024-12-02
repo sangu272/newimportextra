36fc44f88a75958331189d1fb2d8858920924f89
⎯꯭̽𓆰𝅃꯭꯭᳚ ༎ࠫ ꯭𓆩꯭᪵𝛅͟𝗔͟ ̬𝗡᳟᪵͟𝗔᳟͟𝗧͟𝗔᳟᪵͟𝗡᳟͟𝗜 ͟𓆪᪵ꪾ ͟𝛊֟፝ؖ۬𝁘𝐁᧘ᴄʜ𝛊֟፝ؖ۬ <sangeeta271713@gmail.com>
2024-12-02 17:05:57 +0530

    Update a_welcome.py

--- plugins/a_welcome.py
+++ plugins/a_welcome.py
@@ -164,7 +164,10 @@ async def greet_new_members(_, member: ChatMemberUpdated):
 
         # Add the modified condition here
         if member.new_chat_member and not member.old_chat_member:
-            welcome_text = f"""{user.mention}** ʜᴏᴡ ᴀʀᴇ ʏᴏᴜ??**"""
+            welcome_text = f"""𝐍ᴀᴍᴇ 🖤 ◂⚚▸ {user.mention}👀\n\n**⛳️ 𝐖ᴇʟᴄᴏᴍᴇ 𝐓ᴏ 𝐎ᴜʀ 𝐆ʀᴏᴜᴘ ⛳️  
+┏━━━━━━━━🧸━━━━━━━┓
+ {chat_name}
+┗━━━━━━━━🧸━━━━━━━┛\n\n➤ 𝐔ꜱᴇʀɴᴀᴍᴇ 🖤  @{user.username}✨\n\n➤ 𝐔ꜱᴇʀ 𝐈ᴅ 🖤  {user.id}🧿\n\n➤ 𝐌ᴇᴍʙᴇʀs 🖤   {count}🍂"""
             await asyncio.sleep(3)
             await userbot.send_message(chat_id, text=welcome_text)
     except Exception as e:
